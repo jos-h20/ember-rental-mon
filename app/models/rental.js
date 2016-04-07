eb6c94e265d3d41ebc50ad69a9bac3e9cb3ae472
Josh Overly and Megan Fayer <megan@>
2016-04-07 15:58:04 -0700

    add google maps and fomat time helper

--- app/models/rental.js
+++ app/models/rental.js
@@ -6,6 +6,9 @@ export default DS.Model.extend({
   type: DS.attr(),
   image: DS.attr(),
   bedrooms: DS.attr(),
-  cost: DS.attr()
+  date_added: DS.attr(),
+  cost: DS.attr(),
+  latitude: DS.attr('number'),
+  longitude: DS.attr('number')
 });
 //this file is blueprint listing out attributes for what all the other rentals should have.  The DS stands for data store.  .attr stands for attribute.  This is a list that we want to save to our store for each rental that we add.  When a DS Model is exported we see in the line 'export default DS.Model.extend'({ it is added to the data store--saved to the database.
