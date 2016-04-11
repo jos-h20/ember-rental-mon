1cf3db74c4270caa7c34d2bf9fbb1c3ec9ab15bd
Josh Overly <josh_overly@yahoo.com>
2016-04-11 11:45:57 -0700

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -46,9 +46,10 @@ module.exports = function(environment) {
     ENV.APP.rootElement = '#ember-testing';
   }
 
-  if (environment === 'production') {
-
-  }
+if (environment === 'production') {
+  ENV.baseURL = '/ember-rental-mon/dist';
+  ENV.locationType = 'hash';
+}
 
   return ENV;
 };
